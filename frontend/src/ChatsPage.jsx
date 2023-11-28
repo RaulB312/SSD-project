120269e005ce92f15398036127e56c97c12ec533
Raul Bărbosu <raul.barbosu@gmail.com>
2023-11-28 23:15:55 +0200

    added frontend

---
+++ frontend/src/ChatsPage.jsx
@@ -0,0 +1,16 @@
+import { PrettyChatWindow } from "react-chat-engine-pretty";
+
+const ChatsPage = (props) => {
+  return (
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <PrettyChatWindow
+        projectId='cca5b503-032c-4fad-8328-055fb6ed3b9d'
+        username={props.user.username}
+        secret={props.user.secret}
+        style={{ height: "100%" }}
+      />
+    </div>
+  );
+};
+
+export default ChatsPage;
